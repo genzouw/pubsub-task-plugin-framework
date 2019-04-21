b1e76eff8ff917d70d1af7d174a8a8138f328df4
Masanori Yoshimoto <masanori.yoshimoto@zenkigen.jp>
2019-04-21 17:27:33 +0900

    Fix lint errors/warnings

--- sample/sample-subscriber-client.go
+++ sample/sample-subscriber-client.go
@@ -1,23 +1,23 @@
-package main
+package sampleSubscriberClient
 
 import (
-  "log"
-  "os"
-  "strconv"
+	"log"
+	"os"
+	"strconv"
 
-  "github.com/zenkigen/pubsub-task-plugin-framework"
+	"github.com/zenkigen/pubsub-task-plugin-framework"
 )
 
 func main() {
-  proj := os.Getenv("GOOGLE_PROJECT_ID")
-  if proj == "" {
-    log.Printf("GOOGLE_PROJECT_ID is not set. ERR:[%v]", os.Stderr)
-    os.Exit(1)
-  }
-  concurrency, err := strconv.Atoi(os.Getenv("SUBSCIBER_CONCURRENCY"))
-  if err != nil || concurrency == 0 {
-    concurrency = 3
-  }
-  subscriber := &pubsubTaskPlugin.Subscriber{"./sample-plugins"}
-  subscriber.Do(proj, "test-topic", "test-subscription", concurrency)
+	proj := os.Getenv("GOOGLE_PROJECT_ID")
+	if proj == "" {
+		log.Printf("GOOGLE_PROJECT_ID is not set. ERR:[%v]", os.Stderr)
+		os.Exit(1)
+	}
+	concurrency, err := strconv.Atoi(os.Getenv("SUBSCIBER_CONCURRENCY"))
+	if err != nil || concurrency == 0 {
+		concurrency = 3
+	}
+	subscriber := &pubsubTaskPlugin.Subscriber{"./sample-plugins"}
+	subscriber.Do(proj, "test-topic", "test-subscription", concurrency)
 }
